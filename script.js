4990c2e3896a6442390278aae0a235ab566c29f6
Den Enoy <132737377+devmork@users.noreply.github.com>
2025-03-18 22:33:30 +0800

    feat: add function goFight

--- script.js
+++ script.js
@@ -141,3 +141,11 @@ function buyWeapon() {}
 function fightSlime() {}
 
 function fightBeast() {}
+
+function goFight() {
+  update(locations[3]);
+  monsterHealth = monsters[fighting].health;
+  monsterStats.style.display = "block";
+  monsterName.innerText = monsters[fighting].name;
+  monsterHealthText.innerText = monsterHealth;
+}
