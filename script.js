c669197b6124b386d7cfa6aa6d1d55153caa1733
Den Enoy <132737377+devmork@users.noreply.github.com>
2025-03-20 23:22:54 +0800

    feat: add attack function

--- script.js
+++ script.js
@@ -149,3 +149,10 @@ function goFight() {
   monsterName.innerText = monsters[fighting].name;
   monsterHealthText.innerText = monsterHealth;
 }
+
+function attack() {
+  text.innerText = "The " + monsters[fighting].name + " attacks.";
+  text.innerText +=
+    " You attack it with your " + weapons[currentWeaponIndex].name + ".";
+  health -= monsters[fighting].level;
+}
