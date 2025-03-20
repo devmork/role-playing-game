2c5725d5c3860eca64f7a27dd0a2eeeb908afb48
Den Enoy <132737377+devmork@users.noreply.github.com>
2025-03-20 23:40:33 +0800

    feat: add logic to defeat monster function

--- script.js
+++ script.js
@@ -170,6 +170,12 @@ function dodge() {
   text.innerText = "You dodge the attack from the " + monsters[fighting].name;
 }
 
-function defeatMonster() {}
+function defeatMonster() {
+  gold += Math.floor(monsters[fighting].level * 6.7);
+  xp += monsters[fighting].level;
+  goldText.innerText = gold;
+  xpText.innerText = xp;
+  update(locations[4]);
+}
 
 function lose() {}
