6d4f97a60a89b734828b32ab68161457413607c2
Den Enoy <132737377+devmork@users.noreply.github.com>
2025-03-22 23:59:47 +0800

    refactor: delete dodge function

--- script.js
+++ script.js
@@ -176,10 +176,6 @@ function attack() {
   }
 }
 
-function dodge() {
-  text.innerText = "You dodge the attack from the " + monsters[fighting].name;
-}
-
 function defeatMonster() {
   gold += Math.floor(monsters[fighting].level * 6.7);
   xp += monsters[fighting].level;
